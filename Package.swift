971fa509c7c74adb83cfc3af1d01a59b8b1e593a
Nguyen Duc Thinh <thinhnguyen12389@gmail.com>
2024-07-30 12:45:22 +0700

    fix: [TCH-02] Fixed supported iOS version (#4)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "trek_libraries",
+    platforms: [
+      .iOS(.v13),
+    ],
     products: [
         .library(
             name: "trek_libraries",
